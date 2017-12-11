c9b006db565bf8bf2de003b799e3ec9884daf5a3
Giovanni Mendes <giovanni.mendes@ezcommerce.local>
2017-12-11 08:20:57 -0200

    build heroku

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const router = require('./app');
 
 // app.set('view engine', 'ejs');
 app.set('view engine', 'html');
-app.use(express.static('public'))
+//app.use(express.static('public'));
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
 
 router(app, '/');
 
 app.get('/', (req, res) => res.render('index.html'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
